26c3f0feed0d5dadfa7b834f141a942e03018f28
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-09-23 17:50:34 +0200

    Installation required libraries

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Card, Container } from 'react-bootstrap';
+import { faHome, faIceCream } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Home</h2>
-    </div>
+    <Container>
+      {/*<Card bg="dark" text="white">
+        <Card.Header>Pocetna stranica</Card.Header>
+         <Card.Body>
+          <Card.Text>Ovde ide sadrzaj</Card.Text>
+        </Card.Body>
+      </Card> */}
+      <FontAwesomeIcon icon={ faIceCream} />
+      <FontAwesomeIcon icon={ faHome} />
+      Home
+    </Container>
   );
 }
 
