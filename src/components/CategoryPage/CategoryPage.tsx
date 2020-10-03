c766fffc8b4ae0f2fd7b4ed7fc8c1f0863f8a1eb
Lazar Velimirovic <lazarvelimirovic43@gmail.com>
2020-10-03 18:49:40 +0200

    Add filter search-working with features

--- src/components/CategoryPage/CategoryPage.tsx
+++ src/components/CategoryPage/CategoryPage.tsx
@@ -1,7 +1,7 @@
-import { faListAlt } from '@fortawesome/free-solid-svg-icons';
+import { faListAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import { Link, Redirect } from 'react-router-dom';
 import api, { ApiResponse } from '../../api/api';
 import { ApiConfig } from '../../config/api.config';
@@ -22,6 +22,21 @@ interface CategoryPageState {
     subcategories?: CategoryType[];
     articles?: ArticleType[];
     message: string;
+    filters: {
+        keywords: string;
+        priceMinimum: number;
+        priceMaximum: number;
+        order: "name asc" | "name desc" | "price asc" | "price desc";
+        selectedFeatures: {
+            featureId: number;
+            value: string;
+        }[];
+    };
+    features: {
+        featureId: number;
+        name: string;
+        values: string[];
+    }[];
 }
 
 interface CategoryDto {
@@ -52,7 +67,21 @@ export class CategoryPage extends Component<CategoryPageProperties> {
         this.state = {
             isUserLoggedIn: true,
             message: '',
+            filters: {
+                keywords: '',
+                priceMinimum: 0.01,
+                priceMaximum: 100000,
+                order: "price asc",
+                selectedFeatures: [],
+            },
+            features: [],
          };
+    }
+
+    private setFeatures(features: any) {
+        this.setState(Object.assign(this.state, {
+            features: features,
+        }));
     }
     
     private setLoginState(isLoggedIn: boolean) {
@@ -103,12 +132,170 @@ export class CategoryPage extends Component<CategoryPageProperties> {
 
                         { this.showSubcategories() }
                         
-                        { this.showArticles() }
+                        <Row>
+                            <Col xs="12" md="4" lg="3">
+                                { this.printFilters() }
+                            </Col>
+                            <Col xs="12" md="8" lg="9">
+                                { this.showArticles() }
+                            </Col>
+                        </Row>
+                        
                     </Card.Body>
                 </Card>
                 
             </Container>
         )
+    }
+
+    private printFilters() {
+        return (
+            <>
+                <Form.Group>
+                    <Form.Label htmlFor="keywords">Search keywords</Form.Label>
+                    <Form.Control type="text" id="keywords" 
+                        value={ this.state.filters?.keywords } 
+                        onChange={(e) => this.filterKeywordsChanged(e as any) } />
+                </Form.Group>
+
+                <Form.Group>
+                    <Row>
+                        <Col xs="12" sm="6">
+                        <Form.Label htmlFor="priceMin">Min price:</Form.Label>
+                            <Form.Control type="number" id="priceMin"
+                                step="0.01" min="0.01" max="99999.99"
+                                value={ this.state.filters?.priceMinimum} 
+                                onChange= {(e) => this.filterPriceMinChanged(e as any)} />
+                        </Col>
+                        
+                        <Col xs="12" sm="6">
+                            <Form.Label htmlFor="priceMax">Max price:</Form.Label>
+                            <Form.Control type="number" id="priceMax"
+                                step="0.01" min="0.02" max="100000"
+                                value={ this.state.filters?.priceMaximum} 
+                                onChange= {(e) => this.filterPriceMaxChanged(e as any)} />
+                        </Col>
+                    </Row>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Control as="select" id="sortOrder" 
+                        value={ this.state.filters?.order }
+                        onChange={ (e) => this.filterOrderChanged(e as any) } >
+                        <option value="name asc">Sort by name - ascending</option>
+                        <option value="name desc">Sort by name - descending</option>
+                        <option value="price asc">Sort by price - ascending</option>
+                        <option value="price desc">Sort by price - descending</option>
+                    </Form.Control>
+                </Form.Group>
+
+                {/* // this.state.filters */}
+                <h4>Features:</h4>
+
+                { this.state.features.map(this.printFeatureFilterComponent, this) }
+                {/* this argument predstavlja ono unutar konteksta funkcije mapiranja */}
+                <Form.Group>
+                    <Button variant="primary" block onClick={() => this.applyFilters()} >
+                        <FontAwesomeIcon icon={ faSearch } /> Search
+                    </Button>
+                </Form.Group>
+            </>
+        );
+    }
+
+    private printFeatureFilterComponent(feature: { featureId: number; name: string; values: string[]; }) {
+        return (
+            <Form.Group>
+                <Form.Label><strong>{ feature.name }:</strong></Form.Label>
+                    { feature.values.map(value =>  this.printFeatureFilterCheckbox(feature, value), this ) }
+                    
+            </Form.Group>
+        );
+    }
+
+    private printFeatureFilterCheckbox(feature: any, value: string ) {
+        return (
+            <>
+                <Form.Check type="checkbox" label={ value } 
+                    value={ value } 
+                    data-feature-id={ feature.featureId }
+                    onChange={ (e: any) => this.featureFilterChanged(e as any)}  /> 
+            </>
+        )
+    }
+
+    private setNewFilter(newFilter: any) {
+        this.setState(Object.assign(this.state, {
+            filter: newFilter,
+        }))
+    }
+    
+    private filterKeywordsChanged(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setNewFilter(Object.assign(this.state.filters, {
+            keywords: event.target.value, // change keywords to this value
+        }));
+    }
+
+    private filterPriceMinChanged(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setNewFilter(Object.assign(this.state.filters, {
+            priceMinimum: Number(event.target.value), 
+        }));
+    }
+
+    private filterPriceMaxChanged(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setNewFilter(Object.assign(this.state.filters, {
+            priceMaximum: Number(event.target.value), 
+        }));
+    }
+
+    private filterOrderChanged(event: React.ChangeEvent<HTMLSelectElement>) {
+        this.setNewFilter(Object.assign(this.state.filters, {
+            order: event.target.value, 
+        }));
+    }
+
+    private featureFilterChanged(event: React.ChangeEvent<HTMLInputElement>) {
+        const featureId = Number(event.target.dataset.featureId);
+        const value = event.target.value;
+
+        if (event.target.checked) {
+            this.addFeatureFilterValue(featureId, value);
+        } else {
+            this.removeFeatureFilterValue(featureId, value);
+        }
+    }
+
+    private addFeatureFilterValue(featureId: number, value: string) {
+        const newSelectedFeatures = [ ... this.state.filters.selectedFeatures];
+        
+        newSelectedFeatures.push({
+            featureId: featureId,
+            value: value,
+        });
+
+        this.setSelectedFeatures(newSelectedFeatures);
+    }
+
+    private removeFeatureFilterValue(featureId: number, value: string) {
+        const newSelectedFeatures = this.state.filters.selectedFeatures.filter(record => {
+           return !(record.featureId === featureId && record.value === value); // return the result of negation of the if in braces
+        });
+
+        this.setSelectedFeatures(newSelectedFeatures);
+    }
+
+    private setSelectedFeatures(newSelectedFeatures: any) {
+        // take old state and replace with new features
+        this.setState(Object.assign(this.state, {
+            filters: Object.assign(this.state.filters, {
+                selectedFeatures: newSelectedFeatures,
+            })
+        }));
+        console.log(this.state);
+    }
+
+    private applyFilters() {
+        this.getCategoryData();
     }
 
     private printOptionalMessage() {
@@ -232,14 +419,42 @@ export class CategoryPage extends Component<CategoryPageProperties> {
             this.setSubcategories(subcategories);
         });
 
+        const orderParts = this.state.filters.order.split(' ');
+        const orderBy = orderParts[0];
+        const orderDirection = orderParts[1].toUpperCase();
+
+        const featureFilters: any[] = [ ];
+
+        for (const item of this.state.filters.selectedFeatures) {
+            let found = false;
+            let foundref = null;
+
+            for (const featureFilter of featureFilters) {
+                if (featureFilter.featureId === item.featureId) {
+                    found = true;
+                    foundref = featureFilter;
+                    break;
+                }
+            }
+
+            if (!found) {
+                featureFilters.push({
+                    featureId: item.featureId,
+                    values: [ item.value ],
+                });
+            } else {
+                foundref.values.push(item.value);
+            }
+        }
+
         api('api/article/search/', 'post', {
             categoryId: Number(this.props.match.params.cId),
-            keywords: "",
-            priceMin: 0.01,
-            priceMax: Number.MAX_SAFE_INTEGER,
-            features: [ ],
-            orderBy: "price",
-            orderDirection: "ASC",
+            keywords: this.state.filters?.keywords,
+            priceMin: this.state.filters?.priceMinimum,
+            priceMax: this.state.filters?.priceMaximum,
+            features: featureFilters,
+            orderBy: orderBy,
+            orderDirection: orderDirection,
             
         })
         .then((res: ApiResponse) => {
@@ -279,6 +494,23 @@ export class CategoryPage extends Component<CategoryPageProperties> {
             });
 
             this.setArticles(articles);
+        })
+        
+        this.getFeatures();
+    }
+
+    getFeatures() {
+        api('api/feature/values/' + this.props.match.params.cId, 'get', {})
+        .then((res: ApiResponse) => {
+            if (res.status === "login") {
+                return this.setLoginState(false);
+            }
+
+            if (res.status === 'error') {
+                return this.setMessage('Request error. Please try to refresh.');
+            }
+
+            this.setFeatures(res.data.features);
         })
     }
 }
